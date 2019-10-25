0bc0bb01d43146c3b8f3e6895828a446525a12e0
jimmyjonezz <jimmyjonezz@bk.ru>
2019-10-25 18:34:46 +0300

    0.2

--- script/enemy.gd
+++ script/enemy.gd
@@ -34,35 +34,44 @@ func die():
 	$raycast.enabled = false
 	$timer.stop()
 	$collision.set_deferred("disabled", true)
+	$area2d/area_collision.set_deferred("disabled", true)
 	$animation.play("die")
-	yield($animation, "animation_finished")
+	#yield($animation, "animation_finished")
 	$dir_timer.stop()
 	_spawn_props()
 	#queue_free()
 	
 func _ready():
 	randomize()
+	#случайное значение шкалы времени для анимации idle
 	$animation.seek(rand_range(0, 0.6))
-	$dir_timer.wait_time = rand_range(1, 5)
+	#время задержки при смене направления flip_h
+	$dir_timer.wait_time = rand_range(3, 5)
 	$timer.start()
 	$dir_timer.start()
 	
-func _physics_process(delta):
+func _process(delta):
+	#луч рэйкаста чего-то касается?
 	var target_dis = $raycast.is_colliding()
+	#если да, то флаг активен и можно стрелять
 	if target_dis:
 		$atention.visible = true
 		if !save:
+			yield(get_tree().create_timer(0.5), "timeout")
 			shoot()
 		shooting = true
 	else:
 		$atention.visible = false
-	
+
+func _physics_process(delta):	
+	#задел на хождение enemy
 	#velocity.x = speed
 	velocity = move_and_slide(velocity * delta)
 	if velocity.length() > 0:
 		$animation.play("idle")
 		
 func _save():
+	#enemy прячется
 	if !save:
 		position.y -= 5
 		$sprite.self_modulate = Color("#393939")
@@ -75,26 +84,33 @@ func _save():
 		#set_physics_process(true)
 	
 func _spawn_props():
+	#после смерти спавним патроны
 	var ammo = Ammo.instance()
 	ammo.set_position(global_position)
 	props.call_deferred("add_child", ammo)
 
 func _on_timer_timeout():
 	shooting = not shooting
+	if $atention.visible:
+		_save()
 
 func _on_dir_timer_timeout():
+	#если не видим игрока, то смотрим в другую сторону
 	if not $atention.visible:
 		$sprite.flip_h = not $sprite.flip_h
 		$raycast.rotation += PI
 	
+		#$pos для спавна пули - стрельба
 		if $pos.position.x == 7:
 			$pos.position.x = -7
 		else:
 			$pos.position.x = 7
-		$timer.wait_time = rand_range(1, 2)
 		
-	if $atention.visible:
-		_save()
+		#вносим разноообрази во время, за которое enemy 
+		#поворачивается в другую сторону
+		$timer.wait_time = rand_range(2, 3)
+	else:
+		$timer.wait_time = rand_range(0.7, 1.4)
 
 func _on_VisibilityNotifier2D_screen_entered():
 	set_physics_process(true)
