f3308d04da8b815346dfc15b71a9252e75a60aad
Jimmy <jimmyjonezz@bk.ru>
2019-11-02 13:36:42 +0300

    Update main.gd

--- script/main.gd
+++ script/main.gd
@@ -5,7 +5,7 @@ onready var camera = $player/pivot/offset/camera as Camera2D
 
 var exit_door_spawn = [Vector2(424, 142), Vector2(1208, 142), 
 		Vector2(1448, 238), Vector2(1784, 382), Vector2(88, 334)]
-var key_spawn = [Vector2(775, 286), Vector2(536, 238), Vector2(1446, 192)]
+var key_spawn = [Vector2(775, 286), Vector2(536, 238), Vector2(1446, 192), Vector2(1640, 96)]
 
 func _ready():
 	set_camera_limits()
@@ -26,16 +26,12 @@ func set_camera_limits() -> void:
 	camera.limit_bottom = map_limits.end.y * map_cellsize.y
 
 func _on_shader_time_timeout():
-	$ui/shader.visible = false
+	$ui/hromatic.visible = false
 	$ui/post_processing_shader.visible = false
-
-#проверочный код для ключа 2 - потом убрать!
-func _on_key2_pickup():
-	var spawn_door_point = exit_door_spawn[randi() % exit_door_spawn.size()]
-	$stuff/door.position = spawn_door_point
-	printt("position door:", spawn_door_point)
+	$ui/shader_swirl.visible = false
 
 func _on_key1_pickup():
 	var spawn_door_point = exit_door_spawn[randi() % exit_door_spawn.size()]
 	$stuff/door.position = spawn_door_point
 	printt("position door:", spawn_door_point)
+	$sound/door.play()
