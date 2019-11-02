d29ddf20c7fe073ea03f23cd89c0195153d5e53d
jimmyjonezz <jimmyjonezz@bk.ru>
2019-11-02 21:54:16 +0300

    0.9.9.1

--- script/enemy_scene.gd
+++ script/enemy_scene.gd
@@ -3,13 +3,13 @@ extends Node2D
 const ENEMY_SCENE = preload("res://scene/enemy.tscn")
 var positions = []
 
-func _ready():
+#спавн врагов после смерти
+func spawn_enemy(value):
+	positions.clear()
 	var childs = get_children()
 	for x in childs:
 		positions.push_back(x)
-
-#спавн врагов после смерти
-func spawn_enemy(value):
+		
 	var new_enemy = ENEMY_SCENE.instance()
 	new_enemy.set_name("enemy" + str(get_instance_id()))
 	for x in positions:
