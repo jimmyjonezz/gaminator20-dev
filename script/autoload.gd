6bd9a3912996aea6c68e0e0e8b6d1e9db5a15971
Jimmy <jimmyjonezz@bk.ru>
2019-11-02 13:44:30 +0300

    Update autoload.gd

--- script/autoload.gd
+++ script/autoload.gd
@@ -1,10 +1,7 @@
 extends Node
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
 var time = 0
-# Called when the node enters the scene tree for the first time.
+
 func _ready():
 	pass # Replace with function body.
 
@@ -12,5 +9,5 @@ func _ready():
 #func _process(delta):
 #	pass
 
-func _physics_process(d):
-	time += d
+func _physics_process(delta):
+	time += delta
